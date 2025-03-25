aa34314a385546b33ef7a0b783dd3ea8c032763e
Em-Esh-Magar <emeshthapamagar@gmail.com>
2025-03-25 19:59:19 +0545

    Added

--- main3.py
+++ main3.py
@@ -36,3 +36,25 @@ print(ef)
 
 # Condition in an array
 print(a[a>5])
+
+a = np.array([[1,2,3],[4,5,6],[7,8,9,]])
+data = a %2 == 0
+print(data)
+
+
+
+#  creating a new array with existing array
+a = np.array([1,2,3,4,5,6,7,8,9])
+b = a[3:8]      # From 3rd index to 7th index. 8th index is not included
+print(b)
+
+
+# vstack and hstack of 2d arrays:
+a = np.array([[1,2],[3,4]])
+b = np.array([[5,6],[7,8]])
+
+c = np.vstack((a,b))
+print(c)
+
+c = np.hstack((a,b))
+print(c)
